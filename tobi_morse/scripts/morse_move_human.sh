002cb36bfe312ece21244d8d3080e344c85751b8
fl <flier@techfak.uni-bielefeld.de>
2017-04-14 20:15:46 +0200

    Added move human script

---
+++ tobi_morse/scripts/morse_move_human.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /opt/ros/kinetic/setup.bash
+
+rostopic pub /morse/human/motion  geometry_msgs/Twist "linear:
+  x: 0.0
+  y: 0.5
+  z: 0.0
+angular:
+  x: 0.0
+  y: 1.0
+  z: 1.0"
