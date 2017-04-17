92c58f8cdd0989acc667d19ed5d4d54d56b8060c
jkummert <jkummert@techfak.uni-bielefeld.de>
2017-04-17 12:21:23 +0200

    move person in bigger circle 2

--- tobi_morse/scripts/morse_move_human.sh
+++ tobi_morse/scripts/morse_move_human.sh
@@ -4,9 +4,9 @@ source /opt/ros/kinetic/setup.bash
 
 rostopic pub /morse/human/motion  geometry_msgs/Twist "linear:
   x: 0.0
-  y: 5
+  y: 0.5
   z: 0.0
 angular:
   x: 0.0
-  y: 1.0
-  z: 1.0"
+  y: 5.0
+  z: 5.0"
